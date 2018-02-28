bf2b64d0b8baf6c5647a5460c97700843494b6ed
Marley <BusyBeeIsBusyWithBeingABee@gmail.com>
2018-02-28 13:37:08 -0500

    Add files via upload

---
+++ src/ch/makery/address/Main.java
@@ -0,0 +1,263 @@
+package ch.makery.address;
+
+import ch.makery.address.model.Student;
+import ch.makery.address.model.StudentListWrapper;
+import ch.makery.address.view.StudentEditDialogController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
+public class Main extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    //@Override
+    //public void start(Stage primaryStage) throws Exception{
+    //    Parent root = FXMLLoader.load(getClass().getResource("view/StudentOverview.fxml"));
+    //    primaryStage.setTitle("Student Overview");
+    //    primaryStage.setScene(new Scene(root, 300, 275));
+    //    primaryStage.show();
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Student Grading");
+
+        initRootLayout();
+
+        showStudentOverview();
+    }
+
+
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Try to load last opened person file.
+        File file = getStudentFilePath();
+        if (file != null) {
+            loadStudentDataFromFile(file);
+        }
+    }
+
+
+    public void showStudentOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/StudentOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    private ObservableList<Student> studentData = FXCollections.observableArrayList();
+
+
+     //Constructor
+
+    public Main() {
+        // Add some sample data
+        studentData.add(new Student("Hans", "Muster"));
+        studentData.add(new Student("Ruth", "Mueller"));
+        studentData.add(new Student("Heinz", "Kurz"));
+        studentData.add(new Student("Cornelia", "Meier"));
+        studentData.add(new Student("Werner", "Meyer"));
+        studentData.add(new Student("Lydia", "Kunz"));
+        studentData.add(new Student("Anna", "Best"));
+        studentData.add(new Student("Stefan", "Meier"));
+        studentData.add(new Student("Martin", "Mueller"));
+    }
+
+    /**
+    // * Returns the data as an observable list of Persons.
+    // *
+    // * @return
+    // */
+    public ObservableList<Student> getStudentData() {
+        return studentData;
+    }
+
+
+    /**
+    // * Opens a dialog to edit details for the specified student. If the user
+    // * clicks OK, the changes are saved into the provided student object and true
+    // * is returned.
+    // *
+    // * @param student the student object to be edited
+    // * @return true if the user clicked OK, false otherwise.
+    // */
+    public boolean showStudentEditDialog(Student student) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/StudentEditDialog.fxml"));
+            AnchorPane page = loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Student");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the student into the controller.
+            StudentEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setStudent(student);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+    // * Returns the person file preference, i.e. the file that was last opened.
+    // *
+    // * @return
+    // */
+    public File getStudentFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+    // * Sets the file path of the currently loaded file. The path is persisted in
+    // * the OS specific registry.
+    // *
+    // * @param file the file or null to remove the path
+    // */
+    public void setStudentFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            // Update the stage title.
+            primaryStage.setTitle("StudentApp - " + file.getName());
+        } else {
+            prefs.remove("filePath");
+
+            // Update the stage title.
+            primaryStage.setTitle("StudentApp");
+        }
+    }
+    /**
+    // * Loads student data from the specified file. The current student data will
+    // * be replaced.
+    // *
+    // * @param file
+   //  */
+    public void loadStudentDataFromFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(StudentListWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            // Reading XML from the file and unmarshalling.
+            StudentListWrapper wrapper = (StudentListWrapper) um.unmarshal(file);
+
+            studentData.clear();
+            studentData.addAll(wrapper.getStudents());
+
+            // Save the file path to the registry.
+            setStudentFilePath(file);
+
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not load data");
+            alert.setContentText("Could not load data from file:\n" + file.getPath());
+
+            alert.showAndWait();
+        }
+    }
+
+    /**
+    // * Saves the current person data to the specified file.
+    // *
+    // * @param file
+    // */
+    public void saveStudentDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(StudentListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Wrapping our person data.
+            StudentListWrapper wrapper = new StudentListWrapper();
+            wrapper.setStudents(studentData);
+
+            // Marshalling and saving XML to the file.
+            m.marshal(wrapper, file);
+
+            // Save the file path to the registry.
+            setStudentFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not save data");
+            alert.setContentText("Could not save data to file:\n" + file.getPath());
+
+            alert.showAndWait();
+        }
+    }
+
+    }
+
+       //how(); Parent root = FXMLLoader.load(getClass().getResource("StudentOverview.fxml"));
+//               // primaryStage.setTitle("Hello World");
+//               // primaryStage.setScene(new Scene(root, 300, 275));
+//               // primaryStage.s
